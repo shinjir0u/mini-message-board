70e9e9685bb4def6340ec06689d8f3e47c24bce6
shinjir0u <htooagwai9@gmail.com>
2025-07-21 13:49:43 +0630

    Implement plain home page

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const express = require("express");
+const indexRouter = require("./routes/indexRoutes");
 require("dotenv").config();
 
 const PORT = process.env.PORT;
-
 const app = express();
+app.set("view engine", "ejs");
 
-app.use("/", (request, response) => {
-  response.end("hello");
-});
+app.use("/", indexRouter);
 
 app.listen(PORT, () =>
   console.log(`Server started at localhost at port ${PORT}`)
