a19da579955cd34d91753c8bb450aab684d24cd1
shinjir0u <htooagwai9@gmail.com>
2025-07-28 11:39:03 +0630

    Implement external database

--- db/pool.js
+++ db/pool.js
@@ -1,6 +1,6 @@
 const { Pool } = require("pg");
 
-const config = {
+const local_config = {
   host: "localhost",
   user: "shinji",
   database: "messages",
@@ -8,4 +8,9 @@ const config = {
   port: 5432,
 };
 
-module.exports = new Pool(config);
+const external_config = {
+  connectionString:
+    "postgresql://shinji:BnZAIf696GGjJfnpg79UnXZBlpgOUawX@dpg-d23g5pje5dus73ajhc4g-a.oregon-postgres.render.com/messages_f1q6?ssl=true",
+};
+
+module.exports = new Pool(external_config);
